0f603fd2da1ca077765c96881e48d3f601aaf799
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-06-03 12:58:58 +0700

    init commit

---
+++ pkg/alpha/go.mod
@@ -0,0 +1,5 @@
+module github.com/fbin243/go-monorepo-lib
+
+go 1.22.3
+
+require github.com/mitchallen/coin v0.3.0
---
+++ pkg/alpha/init.go
@@ -0,0 +1,14 @@
+/**
+ * Author: Mitch Allen
+ * File: init.go
+ */
+
+package alpha
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("[go-lib] initializing ...")
+}
